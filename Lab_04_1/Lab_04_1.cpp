74f4cc9f895f41992957a3366f975c28843833a6
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2022-10-18 19:29:08 +0300

    Lab_04_1 commit

---
+++ Lab_04_1/Lab_04_1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int N, j;
+	double S;
+	cout << "N = "; cin >> N;
+	S = 0;
+	j = 2;
+	while (j <= N)
+	{
+		S += (j * (N - j)) / (j * j + (N - j) * (N - j));
+		j++;
+	}
+	cout << S << endl;
+	S = 0;
+	j = 2;
+	do {
+		S += (j * (N - j)) / (j * j + (N - j) * (N - j));
+		j++;
+	} while (j <= N);
+	cout << S << endl;
+	S = 0;
+	for (j = 2; j <= N; j++)
+	{
+		S += (j * (N - j)) / (j * j + (N - j) * (N - j));
+	}
+	cout << S << endl;
+	S = 0;
+	for (j = N; j >= 2; j--)
+	{
+		S += (j * (N - j)) / (j * j + (N - j) * (N - j));
+	}
+	cout << S << endl;
+	return 0;
+}
